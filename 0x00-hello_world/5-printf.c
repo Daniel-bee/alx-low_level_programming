fad924c30545706bf83f3be50bdae87e37d2701e
Daniel-bee <daniel123_86@live.com>
2021-06-17 14:35:54 +0300

    C file printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include<stdio.h> 
+/**
+ * main - start here
+ * printf - Prints "puzzle"
+ * return - return zero
+ * Return: 0
+ */ 
+int main(void)
+{ 
+printf("Programming is like building a multilingual puzzle,");
+return (0);
+}
