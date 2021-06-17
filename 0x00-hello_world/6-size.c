07ff08632c81779c2717c080fded37fb43ab709a
Daniel-bee <daniel123_86@live.com>
2021-06-17 15:50:31 +0300

    C file size of data types

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,5 +12,10 @@ printf("Size of an int: %ld bytes\n", sizeof(int));
 printf("Size of a long int: %ld bytes\n", sizeof(long int));
 printf("Size of a long long int: %ld bytes\n", sizeof(long long int));
 printf("Size of a float: %ld bytes\n", sizeof(float));
+printf("Size of a char: Size of a char: %ld bytes\n", sizeof(char));
+printf("Size of an int: %ld bytes\n", sizeof(int));
+printf("Size of a long int: %ld bytes\n", sizeof(long int));
+printf("Size of a long long int: %ld bytes\n", sizeof(long long int));
+printf("Size of a float: %ld bytes\n", sizeof(float));
 return (0);
 }
