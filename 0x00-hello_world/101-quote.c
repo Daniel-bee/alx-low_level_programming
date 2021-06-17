e293d2ae2ad81d245f39d383e9e774aaef78341e
Daniel-bee <daniel123_86@live.com>
2021-06-17 18:32:11 +0300

    C file unix is simple

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -13,10 +13,10 @@ char hello[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 char *s;
 int out;
 s = hello;
-while(*s != '\0' )
+while( *s != '\0' )
 {
 out = putchar(*s);
-if(out == EOF || ferror(stdout) )
+if( out == EOF || ferror(stdout) )
 {
 puts("\nFile error");
 clearerr(stdout);
@@ -24,6 +24,5 @@ return (1);
 }
 s++;
 }
-
 return (1);
 }
