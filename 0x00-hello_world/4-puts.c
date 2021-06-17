c94099300ae4b879388d2be02d37e0bc234fd436
Daniel-bee <daniel123_86@live.com>
2021-06-17 12:44:34 +0300

    C file puts function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,5 @@
 #include<stdio.h>
-/* main function */
-int main(void)
+int main(void)/* main function */
 {
 /* puts function diplay string */
 puts("\"Programming is like building a multilingual puzzle");
