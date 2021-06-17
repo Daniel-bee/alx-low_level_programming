a785b9e4c9726032074684881980cb3b00fe4e0e
Daniel-bee <daniel123_86@live.com>
2021-06-17 16:48:34 +0300

    C file unix is simple

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+/**
+ * main - start here
+ * printf - Prints "Size"
+ * return - return zero
+ * Return: 
+ */
+int main(void)
+{
+printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+return ();
+}
