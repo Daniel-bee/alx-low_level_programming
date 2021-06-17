c18c88a68a7e151ed97d0c07db5fa163d22e8651
Daniel-bee <daniel123_86@live.com>
2021-06-17 16:59:33 +0300

    C file unix is simple

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,6 +7,6 @@
  */
 int main(void)
 {
-printf("%s", "and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+putchar("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
 return ();
 }
