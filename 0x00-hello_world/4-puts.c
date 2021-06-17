791827f6b871560aab58b6ae2f9a4a7a4e958d5e
Daniel-bee <daniel123_86@live.com>
2021-06-17 11:58:47 +0300

    C file puts function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 /**
  * This program use puts function to display string on the screen.
 */
-#include<stdio.h> 
-int main()  
+#include <stdio.h> 
+int main ()  
 { 
-   puts("\"Programming is like building a multilingual puzzle");
-    
+   puts ("\"Programming is like building a multilingual puzzle");
+
    return 0; 
 }
