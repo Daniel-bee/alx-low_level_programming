a18ee7134532e5589cf33e325984c9dc7a51ca35
Daniel-bee <daniel123_86@live.com>
2021-06-17 18:12:35 +0300

    C file unix is simple

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,16 +1,17 @@
 #include<stdio.h>
 /**
  * main - start here
- * printf - Prints "Size"
- * return - return zero
- * Return: 
+ * @hello[]: Initalize array of char
+ * @*s: Pointer
+ * @out : hold the result
+ * return - return 1
+ * Return: 1
  */
 int main()
 {
 char hello[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 char *s;
 int out;
-
 s = hello;
 while( *s != '\0' )
 {
