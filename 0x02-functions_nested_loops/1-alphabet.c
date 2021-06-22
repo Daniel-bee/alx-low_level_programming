59a81ec1c8aa48bb5ee90fe07fe5be0a404d2a5b
Daniel-bee <daniel123_86@live.com>
2021-06-22 06:47:44 -0700

    Lowercase

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,13 +1,6 @@
 #include "holberton.h"
 /**
- *_putchar - writes the character c to stdout
- * @c: The character to print
- *Return: return string characters
-*/
-/*int _putchar(char c);*/
-/**
  *print_alphabet - writes the lower alphabet c to stdout
- *Return: return string characters
 */
 void print_alphabet(void)
 {
