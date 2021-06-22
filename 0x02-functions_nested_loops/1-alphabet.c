6d8a29a3ddc47c2c1cb5c8c32ee99c5a95467069
Daniel-bee <daniel123_86@live.com>
2021-06-22 04:11:26 -0700

    Lower case alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,30 @@
+#include <holberton.h>
+/**
+ *_putchar - writes the character c to stdout
+ * @c: The character to print
+ *Return: return string characters
+*/
+int _putchar(char c);
+/**
+ *print_alphabet - writes the lower alphabet c to stdout
+ *Return: return string characters
+*/
+void print_alphabet(void)
+{
+	int c;
+
+	for (c = 97; c <= 122; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
+/**
+ *main - count characters and print as string result
+ *Return: void
+ */
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
