8f8965d0fa902566c236f36344b6adda2aa1b1cc
Daniel-bee <daniel123_86@live.com>
2021-06-22 05:05:48 -0700

    Lowercase

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,17 +1,15 @@
-#include <unistd.h"
 #include "holberton.h"
-/**
- *print_alphabet - writes the lower alphabet c to stdout
- *Return: return string characters
-*/
-void print_alphabet();
 /**
  *_putchar - writes the character c to stdout
  * @c: The character to print
  *Return: return string characters
 */
 int _putchar(char c);
-
+/**
+ *print_alphabet - writes the lower alphabet c to stdout
+ *Return: return string characters
+*/
+void print_alphabet();
 /**
  *main - count characters and print as string result
  *Return: void
