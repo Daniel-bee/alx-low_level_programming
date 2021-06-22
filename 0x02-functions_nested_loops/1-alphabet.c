6aa6666a629564dfccb62234b3ed0b5072ad29fe
Daniel-bee <daniel123_86@live.com>
2021-06-22 05:17:46 -0700

    Lowercase

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -4,12 +4,12 @@
  * @c: The character to print
  *Return: return string characters
 */
-//int _putchar(char c);
+/*int _putchar(char c);*/
 /**
  *print_alphabet - writes the lower alphabet c to stdout
  *Return: return string characters
 */
-//void print_alphabet();
+/*void print_alphabet();*/
 /**
  *main - count characters and print as string result
  *Return: void
