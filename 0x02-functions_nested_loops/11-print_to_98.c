66cb4b6c210445f22fbdf9ee385fd34f13aa1c29
Daniel-bee <daniel123_86@live.com>
2021-06-22 12:09:06 -0700

    print to 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+/**
+ *print_to_98- print 98
+ *@n: accept an integer
+ *Return: the sum of 2 numbers
+ */
+void print_to_98(int n)
+{
+int i;
+
+	if (n <= 98)
+	{
+		for (i = n; i < 98; i++)
+		{
+			printf("%i, ", i);
+		}
+	} else
+	{
+		for (i = n; i > 98; i--)
+		{
+			printf("%i, ", i);
+		}
+	}
+	printf("\n");
+
+}
