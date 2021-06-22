1e1bdb8b987a6cf9c81ab37f41de97bf317d7207
Daniel-bee <daniel123_86@live.com>
2021-06-22 11:56:48 -0700

    lastdigit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,18 +1,34 @@
 #include "holberton.h"
 /**
+ *_abs - compute absolute value of a number
+ * @n: accept an integer
+ *Return: the absolute value of a number
+ */
+int _abs(int n)
+{
+	if (n > 0)
+	{
+		return (n);
+	}
+	else if (n == 0)
+	{
+		return (0);
+	}
+	else
+	{
+		return (n * -1);
+	}
+}
+/**
  *print_last_digit - compute the last digit of the number
  * @n: accept an integer
  *Return: returns the last gigit of a number
  */
 int print_last_digit(int n)
 {
-	if (n < 0)
-	{
-	  n = _putchar(((n % 10) * -1) + 48);
-	}
-	else
-	{
-		n = _putchar((n % 10) + '0');
-	}
-	return (n);
+	int last;
+	
+	last = _abs(n % 10);
+	_putchar(last + '0');
+	return (last);
 }
