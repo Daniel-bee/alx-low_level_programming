a079ab38edef3772dce9568bbe667c8e0b98c8d0
Daniel-bee <daniel123_86@live.com>
2021-06-22 05:25:17 -0700

    Header

--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -1,12 +1,5 @@
 #include <unistd.h>
-int _putchar(char c);
-void print_alphabet(void)
+int _putchar(char c)
 {
-	char d;
-
-	for (d = 97; d <= 122; d++)
-	{
-		_putchar(d);
-	}
-	_putchar('\n');
+	return (write(1, &c, 1));
 }
