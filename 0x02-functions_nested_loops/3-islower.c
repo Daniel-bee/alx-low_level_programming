ea0a90e31daadb8508e83d161c9cf24ae0b2933a
Daniel-bee <daniel123_86@live.com>
2021-06-22 07:49:28 -0700

    check for lowercase

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,11 +1,12 @@
 #include "holberton.h"
 /**
  *_islower - checks if lawcase or not
- *Return- 1 if lowercase else 0
+ * @c : accept an input character
+ *Return:1 if lowercase else 0
  */
 int _islower(int c)
 {
-	
+
 	if (c >= 97 && c <= 122)
 		return (1);
 	else
