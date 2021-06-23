ac965d605f1248c1c11ae389f1636f5b7064dfb3
Daniel-bee <daniel123_86@live.com>
2021-06-23 04:21:34 -0700

    Time table

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,32 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+*print_times_table- compute time table
+*/
+void print_times_table(int n)
+{
+	int i, j, pro;
+
+	for (i = 0; i <= n; i++)
+	{
+		for (j = 0; j <= n; j++)
+		{
+			pro = i * j;
+			if (pro == 0)
+			{
+				if (i == 0 && j >= 1)
+					printf("%5d", pro);
+				else
+					printf("%d", pro);
+			}
+			else
+			{
+				printf("%5d", pro);
+			}
+			if (j < n)
+				printf(",");
+			else
+				_putchar('\n');
+		}
+	}
+}
