444a268ca99a34015d47f5027efbfbb2441e2e04
Daniel-bee <daniel123_86@live.com>
2021-06-23 05:44:43 -0700

    time table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -7,12 +7,12 @@
 */
 void space(int val)
 {
-	if (val <= 9)
+	if (val <= 9 && val <= 99 )
 	{
-		 _putchar(' ');
 	     _putchar(' ');
 	     _putchar(' ');
-		printf("%d", val);
+	     _putchar(' ');
+	     printf("%d", val);
 		
 	}
 	else if (val > 9 && val <= 99)
