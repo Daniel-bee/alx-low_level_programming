ad49162c55f024b3f6b1a2f8e12c2b450af74fcb
Daniel-bee <daniel123_86@live.com>
2021-06-24 09:00:49 -0700

    Largest prime

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+* main - max prime number
+* Return: 0
+*/
+int main(void)
+{
+	long long int n;
+
+	n = 612852475143;
+	long int div = 2, ans = 0, maxFact;
+
+	while (n != 0)
+	{
+		if (n % div != 0)
+			div = div + 1;
+		else
+		{
+			maxFact = n;
+			n = n / div;
+			if (n == 1)
+			{
+				printf("%lld \n", maxFact);
+				ans = 1;
+				break;
+			}
+		}
+	}
+	return (0);
+}
