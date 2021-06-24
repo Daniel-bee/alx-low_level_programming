94be03a51618af6aa761b3281311a33fa1730cbc
Daniel-bee <daniel123_86@live.com>
2021-06-24 09:34:21 -0700

    Prime factor

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,30 +1,27 @@
-#include <stdio.h>
 /**
-* main - max prime number
-* Return: 0
-*/
+ * main - max prime number
+ * Return: 0
+ */
 int main(void)
 {
-	unsigned long int n;
+  unsigned long  int n = 612852475143;
+  int div = 2, ans = 0, maxFact;
 
-	n = 612852475143;
-	int div = 2, ans = 0, maxFact;
-
-	while (n != 0)
+  while (n != 0)
+    {
+      if (n % div != 0)
+	div = div + 1;
+      else
 	{
-		if (n % div != 0)
-			div = div + 1;
-		else
-		{
-			maxFact = n;
-			n = n / div;
-			if (n == 1)
-			{
-				printf("%d \n", maxFact);
-				ans = 1;
-				break;
-			}
-		}
+	  maxFact = n;
+	  n = n / div;
+	  if (n == 1)
+	    {
+	      printf("%lu\n", maxFact);
+	      ans = 1;
+	      break;
+	    }
 	}
-	return (0);
-}
+    }
+  return (0);
+}~
