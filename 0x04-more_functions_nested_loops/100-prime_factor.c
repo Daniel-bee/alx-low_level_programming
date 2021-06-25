7aabf1bb15fbedeb582be15e56ec0a912be36083
Daniel-bee <daniel123_86@live.com>
2021-06-25 13:51:52 +0000

    Prime factor

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -10,40 +10,13 @@ int main(void)
 	int i = 3;
 
 	while (i <= prime / 2)
+	{
 		if (prime % i == 0)
 			prime = prime / i;
 		else
 			i += 2;
-
-	printf("%ld\n", prime);
+	}
+	printf("%d\n", prime);
 
 	return (0);
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-			}
-
-
-
-
