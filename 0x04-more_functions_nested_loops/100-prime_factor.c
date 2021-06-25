18d4cc86356f92925a659548dabdf97b4346e246
Daniel-bee <daniel123_86@live.com>
2021-06-25 12:05:31 +0000

    Largest prime number

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -6,30 +6,19 @@
  */
 int main(void)
 {
-	long int n;
-	long int max;
-	long int i;
+	unsigned int long n = 612852475143, a = (int) sqrt(n);
 
-	n = 612852475143;
-	max = -1;
+while (1)
+{
 
-	while (n % 2 == 0)
+	if (n % a == 0)
 	{
-		max = 2;
-		n /= 2;
+		printf("%lu \n", n / a);
+		break;
 	}
+	a--;
 
-	for (i = 3; i <= sqrt(n); i = i + 2)
-	{
-		while (n % i == 0)
-		{
-			max = i;
-			n = n / i;
-		}
-	}
-
-	if (n > 2)
-		max = n;
+}
 
 	printf("%ld\n", max);
 	return (0);
