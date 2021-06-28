fc8899698fecdc6f61d139e3d9c93f78edd9d147
Daniel-bee <daniel123_86@live.com>
2021-06-28 09:19:03 +0000

    Print revers string

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,32 @@
+#include "holberton.h"
+/**
+ * _strlen - string length
+ * @s: characters pointer
+ * Return: string length
+ */
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (s[len] != '\0')
+	{
+		len++;
+	}
+	return (len);
+}
+/**
+ * _strlen - print string in revers order
+ * @s: characters pointer
+ */
+void print_rev(char *s)
+{
+	int len = _strlen(s) - 1;
+
+	while (len >= 0)
+	{
+		_putchar(s[len]);
+		len--;
+	}
+	_putchar('\n');
+}
+
