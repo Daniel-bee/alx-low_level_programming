388d95c4ca80b5fd4c219098ce57788709b63724
Daniel-bee <daniel123_86@live.com>
2021-06-28 12:52:10 +0000

    print array

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * print_array - print array value
+ * @a: characters pointer
+ * @b: array length
+ */
+void print_array(int *a, int n)
+{
+	int count = 0;
+
+	while (count < n)
+	{
+		printf("%d", a[count]);
+		if (count < n - 1)
+			printf(", ");
+		count++;
+	}
+	printf("\n");
+}
