bbf7e2d2446311b2ea53721364e6f07891dab66a
Daniel-bee <daniel123_86@live.com>
2021-06-30 17:32:32 +0000

    revers array

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * reverse_array - print assay in revers
+ * @a: integer array
+ * @n: integer
+ */
+void reverse_array(int *a, int n)
+{
+	int i;
+
+	for (i = n - 1; i >= 0; i--)
+	{
+		printf("%d", a[i]);
+		if (i > 0)
+			printf(", ");
+	}
+	printf("\n");
+}
