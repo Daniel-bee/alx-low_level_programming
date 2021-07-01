dbc0995f921310b96ff3d4f9f51b39abe5156833
Daniel-bee <daniel123_86@live.com>
2021-07-01 09:02:45 +0000

    string cmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -7,15 +7,14 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int i;
+	int i, sum = 0;
 
 	for (i = 0; s1[i] != '\0'; i++)
 	{
 		if (s1[i] != s2[i])
-			return (s1[i] - s2[i]);
-		if (s1[i] == s2[i])
-			return (s1[i] - s2[i]);
+			sum = s1[i] - s2[i];
 		if (s1[i] > s2[i])
-			return (s1[i] - s2[i]);
+			sum = s1[i] - s2[i];
 	}
+	return (sum);
 }
