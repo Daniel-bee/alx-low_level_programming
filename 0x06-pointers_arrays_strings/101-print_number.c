f19acb1ecbd7d4eb59af5beab766fd9d983fdfb5
Daniel-bee <daniel123_86@live.com>
2021-07-01 17:19:35 +0000

    Print integer putchar

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+/**
+ * print_number - print integer
+ *@n: integer
+ */
+void print_number(int n)
+{
+	unsigned int num;
+
+	if (n == 0)
+		_putchar('0');
+	if (n < 0)
+	{
+		_putchar('-');
+		num = -n;
+	}
+	else
+	{
+		num = n;
+	}
+	while (num != 0)
+	{
+		_putchar(num % 10 + '0');
+		num = num / 10;
+	}
+}
