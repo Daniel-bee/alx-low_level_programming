15c000887d9b7adc85e353635b6d13b7e48e276a
Daniel-bee <daniel123_86@live.com>
2021-07-01 18:10:04 +0000

    romantic character

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,78 +1,104 @@
 #include "holberton.h"
-#include <stdio.h>
 
 /**
- * isPrintableASCII - determines if n is a printable ASCII char
- * @n: integer
- * Return: 1 if true, 0 if false
+ * _strlen - returns the length of a string
+ * @s: string s
+ * Return: length of string
  */
-int isPrintableASCII(int n)
+int _strlen(char *s)
 {
-	return (n >= 32 && n <= 126);
+	char *p = s;
+
+	while (*s)
+		s++;
+	return (s - p);
 }
 
 /**
- * printHexes - print hex values for string b in formatted form
- * @b: string to print
- * @start: starting position
- * @end: ending position
+ * rev_string - reverses a string
+ * @s: string s
  */
-void printHexes(char *b, int start, int end)
+void rev_string(char *s)
 {
 	int i = 0;
+	int size = _strlen(s);
+	char temp;
 
-	while (i < 10)
+	while (i < size)
 	{
-		if (i < end)
-			printf("%02x", *(b + start + i));
-		else
-			printf("  ");
-		if (i % 2)
-			printf(" ");
+		temp = *(s + i);
+		*(s + i) = *(s + size - 1);
+		*(s + size - 1) = temp;
 		i++;
+		size--;
 	}
 }
 
 /**
- * printASCII - print ascii values for string b,
- * formatted to replace nonprintable chars with '.'
- * @b: string to print
- * @start: starting position
- * @end: ending position
+ * returnRes - changes pretotal to digit to be added
+ * @sum: pre-total
+ * @plusOne: flag to add one to res
+ * Return: returns digit to be placed into array
  */
-void printASCII(char *b, int start, int end)
+int returnRes(int sum, int plusOne)
 {
-	int ch, i = 0;
+	int res;
 
-	while (i < end)
-	{
-		ch = *(b + i + start);
-		if (!isPrintableASCII(ch))
-			ch = 46;
-		printf("%c", ch);
-		i++;
-	}
+	if (sum == 9 && plusOne)
+		res = 0;
+	else if ((sum >= 10 && plusOne) || (sum < 9 && plusOne))
+		res = (sum % 10) + 1;
+	else
+		res = sum % 10;
+	return (res);
 }
 
 /**
- * print_buffer - prints a buffer
- * @b: string
- * @size: size of buffer
+ * returnPlusOne - determines bool of plusOne
+ * @sum: pre-total
+ * @plusOne: flag to add one to res
+ * Return: 1 if true, 0 if false
  */
-void print_buffer(char *b, int size)
+int returnPlusOne(int sum, int plusOne)
 {
-	int start, end;
+	if (sum > 9)
+		plusOne = 1;
+	else if (sum == 9 && plusOne)
+		plusOne = 1;
+	else
+		plusOne = 0;
+	return (plusOne);
+}
 
-	if (size > 0)
+/**
+ * infinite_add - function that adds two numbers
+ * @n1: first number
+ * @n2: second number
+ * @r: buffer that the function will use to store the result
+ * @size_r: size of buffer
+ * Return: pointer to result
+ */
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
+{
+	int sum, res, first, second, i = 0, plusOne = 0;
+	int len1 = _strlen(n1), len2 = _strlen(n2);
+	char *ptr = r;
+
+	while (len1 > 0 || len2 > 0)
 	{
-		for (start = 0; start < size; start += 10)
-		{
-			end = (size - start < 10) ? size - start : 10;
-			printf("%08x: ", start);
-			printHexes(b, start, end);
-			printASCII(b, start, end);
-			printf("\n");
-		}
-	} else
-		printf("\n");
+		first = len1 > 0 ? (*(n1 + len1 - 1) - '0') : 0;
+		second = len2 > 0 ? (*(n2 + len2 - 1) - '0') : 0;
+		sum = first + second;
+		res = returnRes(sum, plusOne);
+		plusOne = returnPlusOne(sum, plusOne);
+		*(ptr + i) = res + '0';
+		len1--;
+		len2--;
+		i++;
+	}
+	if (plusOne)
+		*(ptr + i) = 1 + '0';
+	ptr[++i] = '\0';
+	rev_string(ptr);
+	return ((size_r > _strlen(ptr)) ? ptr : 0);
 }
