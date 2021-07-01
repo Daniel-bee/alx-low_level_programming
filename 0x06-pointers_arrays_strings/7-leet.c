d4f5be666c37025f206dbc2971276dc78451234b
Daniel-bee <daniel123_86@live.com>
2021-07-01 07:06:35 +0000

    string to integer

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,25 @@
+#include "holberton.h";
+/**
+  *leet - convert to integer
+  *@c: character string
+  *Return: char
+  */
+char *leet(char *c)
+{
+	int i, j = 0;
+
+	for (i = 0; c[i] != '\0'; i++, j++)
+	{
+		if (c[i] == 65 || c[i] == 97)
+			c[j] = '4';
+		if (c[i] == 69 || c[i] == 101)
+			c[j] = '3';
+		if (c[i] == 79 || c[i] == 111)
+			c[j] = '0';
+		if (c[i] == 84 || c[i] == 116)
+			c[j] = '7';
+		if (c[i] == 76 || c[i] == 124)
+			c[j] = '1';
+	}
+	return (c);
+}
