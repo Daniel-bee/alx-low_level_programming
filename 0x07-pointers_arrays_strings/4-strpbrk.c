0880edb4b337586448916b7e53cfb0372c23a207
Daniel-bee <daniel123_86@live.com>
2021-07-05 14:42:12 +0000

    strpbrk

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,44 @@
+#include "holberton.h"
+#include <stddef.h>
+/**
+* _strchr - locates a character in a string
+* @s: pointer string
+* @c: character
+* Return: return char
+*/
+char *_strchr(char *s, char c)
+{
+	int ch;
+
+		while (*s)
+		{
+			ch = *s++;
+			if (ch == c)
+			{
+				return (s - 1);
+			}
+			if (ch == 0)
+			{
+				return (NULL);
+			}
+		}
+	return (NULL);
+}
+char *_strpbrk(char *s, char *accept)
+{
+	if ((s == NULL) || (accept == NULL))
+		return (NULL);
+	while (*s)
+	{
+
+		if (_strchr(accept, *s))
+		{
+			return (s);
+		}
+		else
+		{
+			s++;
+		}
+	}
+	return (NULL);
+}
