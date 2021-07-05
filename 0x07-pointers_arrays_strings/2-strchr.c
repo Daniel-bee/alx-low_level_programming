85b3e8b7778e0ed7a5a95f4c2b3d789c5b8060a7
Daniel-bee <daniel123_86@live.com>
2021-07-05 12:22:24 +0000

    strchar

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+* _strchr - locates a character in a string
+* @s: pointer string
+* @c: character
+* Return: return char
+*/
+char *_strchr(char *s, char c)
+{
+	char *tmp = (char *) s;
+
+	while (*tmp)
+	{
+		if (*tmp == c)
+			return (tmp);
+		tmp++;
+	}
+	return (0);
+}
