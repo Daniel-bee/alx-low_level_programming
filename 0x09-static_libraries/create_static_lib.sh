1c5a935ff27ec9a70c52d179a5b49d55e479e019
Daniel-bee <84592600+Daniel-bee@users.noreply.github.com>
2021-07-09 12:20:13 +0300

    Create static library

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -2,7 +2,13 @@
 /**
 * this line create an object for all c file in current folder
 */
-gcc -c -Wall -Werror -Wextra *.c
+gcc -c -Wall -Werror -Wextra *.C
+/**
+* this line create an object for all c file in current folder
+*
 ar -rc liball.a *.o
+/**
+* this line create an object for all c file in current folder
+*
 ranlib lowball.a
 
