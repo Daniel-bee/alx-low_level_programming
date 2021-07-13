76da8d7c3370f75d0a877e8ba0e7363ebf197982
Daniel-bee <daniel123_86@live.com>
2021-07-13 08:10:29 +0000

    array of chars

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -11,6 +11,8 @@ char *create_array(unsigned int size, char c)
 	char *arr = (char *)malloc(size * sizeof(c));
 	unsigned int i;
 
+	if (size == 0 || arr == NULL)
+		return (NULL);
 	for (i = 0; i < size; i++)
 	{
 		arr[i] = c;
