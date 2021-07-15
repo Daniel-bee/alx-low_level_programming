c3472b28c49ee41523f9530ad3270b5ed77d8695
Daniel-bee <84592600+Daniel-bee@users.noreply.github.com>
2021-07-15 20:30:32 +0300

    range

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -9,22 +9,7 @@
 */
 int *array_range(int min, int max)
 {
-int *m;
-int i;
-
-if (min > max)
-	return (NULL);
-
-m = malloc((max - min + 1) * sizeof(int));
-
-if (m == 0)
-	return (NULL);
-
-
-for (i = 0; min + i <= max; i++)
-	m[i] = min + i;
-
-return (m);int i, l;
+	int i, l;
 	int *a;
 
 	if (min > max)
