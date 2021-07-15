32e6624bae370ee9ae0ab0ff12d797880312ced8
Daniel-bee <daniel123_86@live.com>
2021-07-15 16:25:57 +0000

    str n cat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -11,7 +11,7 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *concat;
-	unsigned int len = n, index;
+	unsigned int len = 0, len1 = 0, index;
 
 	if (s1 == NULL)
 		s1 = "";
@@ -21,8 +21,12 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	for (index = 0; s1[index]; index++)
 		len++;
-
-	concat = malloc(sizeof(char) * (len + 1));
+	for (index = 0; s2[index]; index++)
+		len1++;
+	if (n >= len1)
+		concat = malloc(sizeof(char) * (len + len1 + 1));
+	else
+		concat = malloc(sizeof(char) * (len + 1));
 
 	if (concat == NULL)
 		return (NULL);
@@ -32,7 +36,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	for (index = 0; s1[index]; index++)
 		concat[len++] = s1[index];
 
-	for (index = 0; s2[index] && index < n; index++)
+	for (index = 0; s2[index] && index < (len + len1); index++)
 		concat[len++] = s2[index];
 
 	concat[len] = '\0';
