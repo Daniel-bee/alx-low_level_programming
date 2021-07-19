3f219ee5e6db43cda0d1bf2d3a1059f645b8de51
Daniel-bee <daniel123_86@live.com>
2021-07-19 12:33:18 +0000

    struct

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -5,11 +5,12 @@
 * @name: string
 * @age : float
 * @owner: char string
-*  Description: dog struct create name, age, and owenr objects
+* Description: dog struct create name, age, and owenr objects
 */
-struct dog{						
-	char *name;	
-	float age;	
+struct dog
+{
+	char *name;
+	float age;
 	char *owner;
 };
 #endif /* DOG */
