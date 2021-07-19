833579dafc0ebdd23b29f18c3ee9ea31eea9efe8
Daniel-bee <daniel123_86@live.com>
2021-07-19 13:27:03 +0000

    init struct

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "holberton.h"
+#include "dog.h"
+/**
+* init_dog - init struct object.
+* @d: struct type
+* @name: name
+* @age: age
+* @owner: awner
+*/
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	 d->name = name;
+	 d->age = age;
+	 d->owner = owner;
+}
