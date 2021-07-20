1d09657ced738b1e9ebd8cb913fffb3ba43d2850
Daniel-bee <daniel123_86@live.com>
2021-07-20 15:37:57 +0000

    array

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,20 @@
+#include "function_pointers.h"
+#include <stdio.h>
+/**
+* array_iterator - function given as a parameter on each element of an array
+* @array: base address
+* @size: array size
+* @action: pointer function action accept an int retun empty
+*/
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int i;
+
+	if (array != NULL || action != NULL)
+	{
+		for (i = 0; i < size; i++)
+		{
+			action(*array++);
+		}
+	}
+}
