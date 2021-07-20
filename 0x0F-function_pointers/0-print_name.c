2926b73136f516e3f098866af1b3c47d9683cc7b
Daniel-bee <daniel123_86@live.com>
2021-07-20 15:32:13 +0000

    print name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,11 @@
+#include "function_pointers.h"
+#include <stdio.h>
+/**
+* print_name - print string
+* @name: pointer name to char
+* @f: pointer function f that accept pointer that point to char
+*/
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
