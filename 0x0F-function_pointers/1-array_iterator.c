c04a6bce21f195c393d85fd580602316aec36289
Daniel-bee <daniel123_86@live.com>
2021-07-20 15:43:02 +0000

    array

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -8,17 +8,12 @@
 */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	unsigned int i;
-
-	if (array != NULL || action != NULL)
-	{
-		for (i = 0; i < size; i++)
-		{
-			action(*array++);
-		}
-	}
-	else
-	{
+	if (array == NULL || action == NULL)
 		return;
+
+	while (size-- > 0)
+	{
+		action(*array);
+		array++;
 	}
 }
