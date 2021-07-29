84cb49bb60603dafa5c8017123d3d4be50f6dc82
Daniel-bee <daniel123_86@live.com>
2021-07-29 13:32:10 +0000

    Print list Updated

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -7,16 +7,17 @@
 size_t print_list(const list_t *h)
 {
 	size_t countnode = 0;
+	const list_t *tmp = h;
 
-	while (h)
+	while (tmp != NULL)
 	{
-		if (h->str == NULL)
+		if (tmp->str == NULL)
 			printf("[0] (nil)\n");
 		else
-			printf("[%d] %s \n", h->len, h->str);
+			printf("[%d] %s \n", tmp->len, tmp->str);
 
 		countnode++;
-		h = h->next;
+		tmp = tmp->next;
 	}
 	return (countnode);
 }
