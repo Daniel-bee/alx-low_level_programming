2d51e9bb70ffc7eabc96b25c22c061102e19bd6c
Daniel-bee <daniel123_86@live.com>
2021-07-29 15:21:42 +0000

    length

---
+++ 1-list_len.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * list_len - number of elements in a linked list_t list
+ * @h: list
+ * Return: length
+ */
+size_t list_len(const list_t *h)
+{
+	size_t nodelen = 0;
+
+	while (h)
+	{
+		nodelen++;
+		h = h->next;
+	}
+	return (nodelen);
+}
