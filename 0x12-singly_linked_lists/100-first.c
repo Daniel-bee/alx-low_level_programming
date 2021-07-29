bd4be7ef636d2d1e406b372828bdc0de4df5f72e
Daniel-bee <daniel123_86@live.com>
2021-07-29 18:16:42 +0000

    function starts before main

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,8 @@
+#include "lists.h"
+void exbm (void) __attribute__ ((constructor));
+
+void exbm (void)
+{
+    printf ("A tortoise, having pretty good sense of a hare's nature, challenges one to a race.\n");
+}
+
