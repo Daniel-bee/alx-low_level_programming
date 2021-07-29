8222a64510ebe8a4639919fd642cb8bec231a75e
Daniel-bee <daniel123_86@live.com>
2021-07-29 18:29:40 +0000

    function starts before mainclear

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -7,7 +7,7 @@ void exbm(void) __attribute__ ((constructor));
  */
 void exbm(void)
 {
-	printf("You're beat! and yet, you must allow,
-		    \nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
 
