849080203453ed2fff16b50d2cf06d316e096ed4
Daniel-bee <daniel123_86@live.com>
2021-08-03 08:32:08 +0000

    add node

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -1,14 +1,15 @@
 #include "lists.h"
 /**
  *add_nodeint- adds a new node at the beginning of a listint_t list
- *@h: list
+ *@head: list
+ *@n: value
  *Return: address
  */
 listint_t *add_nodeint(listint_t **head, const int n)
 {
 	listint_t *tmp;
 
-	tmp = (listint_t*)malloc(sizeof(listint_t));
+	tmp = (listint_t *)malloc(sizeof(listint_t));
 
 	if (tmp == NULL)
 		return (NULL);
@@ -16,6 +17,5 @@ listint_t *add_nodeint(listint_t **head, const int n)
 	tmp->n = n;
 	tmp->next = (*head);
 	(*head) = tmp;
-	
 	return (*head);
 }
