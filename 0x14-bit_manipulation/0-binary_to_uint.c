9b0e47a71712c67703069d1253555eb3b952ca72
Daniel-bee <84592600+Daniel-bee@users.noreply.github.com>
2021-08-05 16:27:18 +0300

    unit

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 unsigned int _pow(unsigned int);
 /**
  * binary_to_uint - onverts a binary number to an unsigned int
@@ -8,39 +7,39 @@ unsigned int _pow(unsigned int);
  */
 unsigned int  binary_to_uint(const char *b)
 {
-        unsigned int j = 0, len, i, num, l = 0;
+	unsigned int j = 0, len, i, num, l = 0;
 
-        if (b == NULL)
-                return (0);
-        while (b[l])
-        {
-                if ((b[l] < 48 || b[l] > 57))
-                        return (0);
-                l++;
-        }
+	if (b == NULL)
+		return (0);
+	while (b[l])
+	{
+		if ((b[l] < 48 || b[l] > 57))
+			return (0);
+		l++;
+	}
 
-        while (b[j])
-                j++;
+	while (b[j])
+	j++;
 
-        len = j - 1;
-        num = (b[0] - '0') * _pow(len);
+	len = j - 1;
+	num = (b[0] - '0') * _pow(len);
 
-        for (i = 1; b[i]; i++)
-                num += ((b[i] - '0') * _pow(--len));
-        return (num);
+	for (i = 1; b[i]; i++)
+		num += ((b[i] - '0') * _pow(--len));
+	return (num);
 }
 /**
- * _pow - perform power of a number
- * @x: value of a given number
- * Return: power of number
- */
+* _pow - perform power of a number
+* @x: value of a given number
+* Return: power of number
+*/
 unsigned int _pow(unsigned int x)
 {
-        unsigned int po = 1;
+	unsigned int po = 1;
 
-        if (x == 0)
-                return (1);
-        while (x--)
-                po *= 2;
-        return (po);
+	if (x == 0)
+		return (1);
+	while (x--)
+		po *= 2;
+	return (po);
 }
