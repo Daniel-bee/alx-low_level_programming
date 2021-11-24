21177e193084d6d2d094a037dba0704c3b424693
Daniel-bee <daniel123_86@live.com>
2021-11-24 20:49:08 +0300

    perimeter

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+def island_perimeter(grid):
+    """
+    grid is a list of list of integers:
+        0 represents a water zone
+        1 represents a land zone
+        One cell is a square with side length 1
+        Grid cells are connected horizontally/vertically (not diagonally).
+        Grid is rectangular, width and height don’t exceed 100
+    """
+    x, y = 0, 0
+    for row in range(1, len(grid) - 1):
+            for col in range(1, len(grid[row]) - 1):
+                if grid[row][col] == 1:
+                    if grid[row][col - 1] == 0:
+                        y += 1
+                    if grid[row][col + 1] == 0:
+                        y += 1
+                    if grid[row - 1][col] == 0:
+                        x += 1
+                    if grid[row + 1][col] == 0:
+                        x += 1
+    return x + y
