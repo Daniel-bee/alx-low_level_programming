d0adb9d0f5064de408299ca0f0f2c1ad441d4b9c
Daniel-bee <daniel123_86@live.com>
2022-01-17 18:21:26 +0300

    interpolation

---
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -0,0 +1,28 @@
+#include "search_algos.h"
+/**
+ *interpolation_search - searches for a value in a sorted array
+ *of integers using the Interpolation search algorithm
+ *@array: list of element
+ *@size: size of an array
+ *@value: value search for
+ *Return: (-1) if array element Null or not found
+ */
+int interpolation_search(int *array, size_t size, int value)
+{
+	size_t low = 0, high = size - 1, pos;
+
+	while (low <= high)
+	{
+		pos = low + (((double)(high - low) /
+			(array[high] - array[low])) * (value - array[low]));
+
+		if (pos > size)
+			return (-1);
+		printf("Value checked array[%ld] = [%d]\n", pos, array[pos]);
+		if (value == array[pos])
+			return (pos);
+		low++;
+	}
+
+	return (-1);
+}
