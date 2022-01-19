3195996dc279f413f1d38c486fb97c8ae14a0135
Daniel-bee <daniel123_86@live.com>
2022-01-19 07:41:04 +0300

    binary search

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -27,8 +27,7 @@ int binary_search(int *array, size_t size, int value)
 
 		if (array[mid] == value)
 			return (mid);
-
-		if (array[mid] < value)
+		else if (array[mid] < value)
 			start = mid + 1;
 		else
 			end = mid - 1;
