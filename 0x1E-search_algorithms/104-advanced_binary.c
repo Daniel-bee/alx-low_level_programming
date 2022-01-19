ad3a1dd02d817bf54480c48a29eef4965db860df
Daniel-bee <daniel123_86@live.com>
2022-01-19 15:57:48 +0300

    advanced binary

--- 0x1E-search_algorithms/104-advanced_binary.c
+++ 0x1E-search_algorithms/104-advanced_binary.c
@@ -9,35 +9,25 @@
  *
  * Return: first index where value is, otherwise -1 if no value or array NULL
  */
-
-int recursive_bsearch(int *array, size_t size, int value, unsigned int index)
+int advanced_binary_recursive(int *array, size_t left, size_t right, int value)
 {
-	unsigned int lo = 0;
-	unsigned int hi = size - 1;
-	unsigned int mid;
+	size_t i;
 
-	if (size == 0)
+	if (right < left)
 		return (-1);
 
 	printf("Searching in array: ");
-	print_array(array, size);
+	for (i = left; i < right; i++)
+		printf("%d, ", array[i]);
+	printf("%d\n", array[i]);
 
-	if (lo > hi)
-		return (-1);
-	if (size % 2 == 0)
-		mid = (size / 2) - 1;
-	else
-		mid = size / 2;
-	if (array[mid] == value)
-		return (mid + index);
-	else if (array[mid] > value)
-		return (recursive_bsearch(array, mid, value, index));
-	else if (array[mid] < value)
-		return (recursive_bsearch(array + mid + 1, size - mid - 1,
-					  value, index + mid + 1));
-	return (-2);
+	i = left + (right - left) / 2;
+	if (array[i] == value && (i == left || array[i - 1] != value))
+		return (i);
+	if (array[i] >= value)
+		return (advanced_binary_recursive(array, left, i, value));
+	return (advanced_binary_recursive(array, i + 1, right, value));
 }
-
 /**
  *  advanced_binary - searches for value in array ints with Binary search algo
  *
@@ -47,34 +37,10 @@ int recursive_bsearch(int *array, size_t size, int value, unsigned int index)
  *
  * Return: first index where value is, otherwise -1 if no value or array NULL
  */
-
 int advanced_binary(int *array, size_t size, int value)
 {
-	unsigned int index = 0;
-
-	if (!array)
+	if (array == NULL || size == 0)
 		return (-1);
 
-	return (recursive_bsearch(array, size, value, index));
-}
-
-/**
- * print_array - Prints an array of integers
- *
- * @array: The array to be printed
- * @size: Number of elements in @array
- */
-void print_array(int *array, size_t size)
-{
-	size_t i;
-
-	i = 0;
-	while (array && i < size)
-	{
-		if (i > 0)
-			printf(", ");
-		printf("%d", array[i]);
-		++i;
-	}
-	printf("\n");
+	return (advanced_binary_recursive(array, 0, size - 1, value));
 }
